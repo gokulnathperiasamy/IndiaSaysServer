9e5783ef5c7e1c42ecd6e90157791b63ecbd9de8
Gokul Nath KP <gokulnath.kpgn@gmail.com>
2017-07-30 10:58:19 +0530

    Update question result table.

--- src/com/kpgn/indiasaysserver/server/QuestionResultMySqlDBServer.java
+++ src/com/kpgn/indiasaysserver/server/QuestionResultMySqlDBServer.java
@@ -1,7 +1,11 @@
 package com.kpgn.indiasaysserver.server;
 
+import java.util.List;
+import java.util.ListIterator;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 import com.kpgn.indiasaysserver.application.QuestionResultConstant;
 import com.kpgn.indiasaysserver.entity.IQuestionResult;
@@ -172,5 +176,45 @@ public class QuestionResultMySqlDBServer extends BaseMySqlDBServer {
 		}
 		return false;
 	}
+	
+	public synchronized boolean updateResult(QuestionResult questionResult) {
+		if (factory == null) createFactory();
+		if (session == null) session = factory.openSession();
+		transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			List<?> questionResultList = session.createCriteria(QuestionResult.class).add(Restrictions.eq("questionID", questionResult.getQuestionID())).list();
+			if (questionResultList != null) {
+				ListIterator<?> questionResultListIterator = questionResultList.listIterator();
+				while (questionResultListIterator != null && questionResultListIterator.hasNext()) {
+					QuestionResult qResult = (QuestionResult) questionResultListIterator.next();
+					qResult.setAgeGroup_00_14(qResult.getAgeGroup_00_14() + questionResult.getAgeGroup_00_14());
+					qResult.setAgeGroup_15_24(qResult.getAgeGroup_15_24() + questionResult.getAgeGroup_15_24());
+					qResult.setAgeGroup_25_34(qResult.getAgeGroup_25_34() + questionResult.getAgeGroup_25_34());
+					qResult.setAgeGroup_35_44(qResult.getAgeGroup_35_44() + questionResult.getAgeGroup_35_44());
+					qResult.setAgeGroup_45_99(qResult.getAgeGroup_45_99() + questionResult.getAgeGroup_45_99());
+					session.update(qResult);
+				}
+			}
+			transaction.commit();
+			return true;
+		} catch (HibernateException he) {
+			if (transaction != null) {
+				transaction.rollback();
+				transaction = null;
+			} 
+			he.printStackTrace();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+				transaction = null;
+			}
+			e.printStackTrace();
+		} finally {
+			if (session != null) { session.close(); session = null; } 
+		}
+		return false;
+	}
+	
 	
 }
