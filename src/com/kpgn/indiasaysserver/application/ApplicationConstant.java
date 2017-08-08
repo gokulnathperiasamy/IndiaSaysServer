aac6c3a8a0dba95991ccc8c315fe5969d074f91e
Gokul Nath KP <gokulnath.kpgn@gmail.com>
2017-08-08 18:07:03 +0530

    Change time to production format. 7 Days.

--- src/com/kpgn/indiasaysserver/application/ApplicationConstant.java
+++ src/com/kpgn/indiasaysserver/application/ApplicationConstant.java
@@ -9,11 +9,11 @@ public abstract class ApplicationConstant {
 	public static final String MySQL_USER_NAME = "root";				// Change in Hibernate CFG XML also.
 	public static final String MySQL_PASSWORD = "root";					// Change in Hibernate CFG XML also.
 	
-	private static final int INVALIDATE_CODE_TIMEOUT_IN_DAYS = 1;		// In Days
+	private static final int INVALIDATE_CODE_TIMEOUT_IN_DAYS = 7;		// In Days
 	
-	// Invalidate question for each INVALIDATE_CODE_TIMEOUT_IN_DAYS in days - Production
-	// public static final int INVALIDATE_CODE_TIMEOUT_IN_MINUTES = INVALIDATE_CODE_TIMEOUT_IN_DAYS * 1440;	// In minutes (24 hours = 1440 minutes)
+	// Invalidate question for each INVALIDATE_CODE_TIMEOUT_IN_DAYS in days - AWS - Production
+	public static final int INVALIDATE_CODE_TIMEOUT_IN_MINUTES = INVALIDATE_CODE_TIMEOUT_IN_DAYS * 1440;	// In minutes (24 hours = 1440 minutes)
 
-	// Invalidate question for each INVALIDATE_CODE_TIMEOUT_IN_DAYS in Minutes - Testing
-	public static final int INVALIDATE_CODE_TIMEOUT_IN_MINUTES = INVALIDATE_CODE_TIMEOUT_IN_DAYS;
+	// Invalidate question for each INVALIDATE_CODE_TIMEOUT_IN_DAYS in Minutes - Local - Testing
+	//public static final int INVALIDATE_CODE_TIMEOUT_IN_MINUTES = INVALIDATE_CODE_TIMEOUT_IN_DAYS;
 }
